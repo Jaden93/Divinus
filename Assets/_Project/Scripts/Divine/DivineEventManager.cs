b68ea7c7e007aaa82bd578c8e52a72f6a66762d9
Gianluca <gianluca.naddeo@scao.it>
2026-04-10 22:06:57 +0200

    feat(divine): add DivineEventManager and DivineEvent struct

---
+++ Assets/_Project/Scripts/Divine/DivineEventManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace DivinePrototype
+{
+    public enum DivineEventType { Smite, Repair, Revive }
+
+    public struct DivineEvent
+    {
+        public DivineEventType Type;
+        public Vector3 Position;
+        public GameObject Target;
+        public float Radius;
+    }
+
+    public static class DivineEventManager
+    {
+        public static event Action<DivineEvent> OnDivineEvent;
+
+        public static void Broadcast(DivineEvent e)
+        {
+            Debug.Log($"[DivineEventManager] Broadcasting {e.Type} at {e.Position}");
+            OnDivineEvent?.Invoke(e);
+        }
+    }
+}
